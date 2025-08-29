f4bca25ef61e685fed3813987f891a074174b3d7
Shradha Chauhan <shradhachauhan31433@gmail.com>
2025-08-29 15:19:52 +0530

    New changes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import SideBar from "../SideBar/SideBar";
 import Users from "../Users/Users";
 import { AppContext } from "../../context/AppContext";
 import "./Dashboard.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import ItemMaster from "../Forms/ItemMaster/ItemMaster";
 import VendorMaster from "../Forms/VendorMaster/VendorMaster";
 import { Link } from "react-router-dom";
@@ -13,9 +13,19 @@ import WarehouseMaster from "../Forms/WarehouseMaster/WarehouseMaster";
 import BOMMaster from "../Forms/BOMMaster/BOMMaster";
 import TypeMaster from "../Forms/TypeMaster/TypeMaster";
 import AdminLandingPage from "../AdminLandingPage/AdminLandingPage";
+import Cookies from "js-cookie";
+import { Navigate } from "react-router-dom";
 
 const Dashboard = () => {
   const { rightSideComponent, setRightSideComponent } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const token = Cookies.get("token");
+  //   if (!token) {
+  //     navigate("/", { replace: true });
+  //   }
+  // }, [navigate]);
 
   return (
     <div className="right-side-div bg-light flex-grow-1 overflow-auto">
